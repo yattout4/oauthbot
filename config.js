d77fe434bbd84d73c45d73fa7927d9ffe36ca173
Younes Attout <leziziatamerelapute@gmail.com>
2023-11-08 16:01:36 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  "token": "MTE3MTU1MTUxMTc2NDAwOTAwMQ.Gvn10K.bGOYBF-SZUoJSdTwrJlc2YgDQIFY6XUdQOBNVo",
+  "client_secret": "3XWmWub-xfBZG0MyNbTbHf2W164c8S9E",
+  "mongodb": "mongodb+srv://doadmin:837fLTWV9Z4o0U25@mongodb-d1a15601.mongo.ondigitalocean.com/admin?tls=true&authSource=admin",
+  "prefix": "-",
+  "owner": "1102535626676510790",
+  "cooldown100": false,
+  "support": "https://discord.gg/gBQchM3AYF",
+  "client_id": "1171551511764009001",
+  "redirect_uri": "https://authorize.oauth2-bot.site/verify",
+  "oauth_link": "https://discord.com/api/oauth2/authorize?client_id=1165385911178100736&redirect_uri=https%3A%2F%2Fauthorize.oauth2-bot.site%2Fverify&response_type=code&scope=identify%20guilds.join",
+  "owners": [
+    "1095041952581423217",
+    "335791240077639691",
+    "1168512773450367118",
+    "990895567645310987"
+  ],
+  "interval": "1000",
+  "bot_link": "https://discord.com/api/oauth2/authorize?client_id=1171551511764009001&permissions=8&scope=bot",
+  "emojis": {
+    "nitro": ">",
+    "check": ">",
+    "arrow": ">",
+    "user": ">",
+    "user_invite": ">",
+    "info": ">",
+    "load": ">",
+    "error": ">",
+    "success": ">",
+    "yes": ">",
+    "link": ">",
+    "reparer": ">"
+  },
+  "webhook": {
+    "avatarURL": "https://cdn.discordapp.com/attachments/1171550574198665257/1171810648141873173/b4eb6270b7f0a825a50a022477b5f313.png?ex=655e08b5&is=654b93b5&hm=5775b21d5c5e749af66e576eb406b2b9b1d9a91749d0047dbc55f046c71ef6c8&",
+    "URL": "https://discord.com/api/webhooks/1171810720359387166/lOiq0HvA0fyUrQGrgojQ62tHdQBarNKUrVpOUdJ19zkkYLrIwSTMAOdQY_0U9JDKZT9I",
+    "webhookNAME": "Wh oauths"
+  },
+  "purchaseDate": "2023-10-24",
+  "expirationDate": "2028-10-25",
+  "buyerName": "pascale2270",
+  "botName": "🍭・e-couple FreeNsfw 🔞,#9776",
+  "infobuy": "Buy to Paypal"
+};
